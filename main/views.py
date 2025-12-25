e9d14cb3d43511d1c7a3f13617e3eb530bf30486
jaehyeonkim <denny1105kim@gmail.com>
2025-12-25 13:58:02 +0900

    [feat] change django url --> api(front:react)
    This patch changes django url --> api.
    
    Signed-off-by: jaehyeonkim <denny1105kim@gmail.com>

--- main/views.py
+++ main/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
-def health_check(request):
-    return render(request, "main/health_check.html")
+def health(request):
+    return JsonResponse({"ok": True})

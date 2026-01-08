c0f2f0b130180a687fcd5b6f3c6ca074d4db40a7
Hoya-0824 <tls980824@gmail.com>
2026-01-08 16:56:56 +0900

    feat: 토큰 갱신 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import GoogleLoginView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('google/', GoogleLoginView.as_view(), name='google_login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
